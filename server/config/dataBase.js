e87f6dfaecc803621be55332b258d0af49bf3f55
Manuel <m.a.c.p90@gmail.com>
2017-12-02 11:04:36 -0500

    riontrello so far

--- server/config/dataBase.js
+++ server/config/dataBase.js
@@ -1,13 +1,16 @@
 
 const mongoose = require('mongoose');
 const dbName = 'irontrello';
+require ("dotenv").config();
+
 
 // connect to the database
 mongoose.connect(`mongodb://localhost/${dbName}` , { useMongoClient: true }); //fixes Deprecation Warning
-
+mongoos4.connect(process.env.MONGODB_URI);
 const db = mongoose.connection
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
   console.log(`Connected to the ${dbName} database`);
 });
+MONGO
